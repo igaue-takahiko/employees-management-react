aa97ff3c1fa099c225e58bc6189974f1d21ed4e7
igaue-takahhiko <hasewo.2610@gmail.com>
2020-11-21 02:29:24 +0900

    implement App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import './App.css';
+import React from 'react';
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+
+import styles from './App.module.css';
+import { Auth, MainPage } from './components';
+
+const client = new ApolloClient({
+    uri: "http://127.0.0.1:8000/graphql/",
+    headers: {
+        Authorization: localStorage.getItem("token")
+            ? `JWT ${localStorage.getItem("token")}`
+            : "",
+    },
+    cache: new InMemoryCache()
+});
 
 const App = () => {
     return (
-        <div>
-        </div>
+        <ApolloProvider client={client}>
+            <div className={styles.app}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path="/" component={Auth} />
+                        <Route exact path="/employees" component={MainPage} />
+                    </Switch>
+                </BrowserRouter>
+            </div>
+        </ApolloProvider>
     );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import { from } from "@apollo/react-hooks"
+
+export { default as Auth } from './Auth'
+export { default as MainPage } from './MainPage'
